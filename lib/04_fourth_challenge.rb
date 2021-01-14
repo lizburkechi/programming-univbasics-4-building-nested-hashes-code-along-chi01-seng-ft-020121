bf7710317d4d918af6a52badee0104d3abb760ef
Liz Burke <liz.burke.chi@gmail.com>
2021-01-14 20:11:55 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,39 +6,33 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
       {
-          :name "Benvolio",
-          :age "17"
-          :attitude "worried"
-        }
-        {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        },
           {
-            :name "Mercutio",
-            :age "18",
-            :attitude "hot-headed"
+            name: "Mercutio",
+            age: "18",
+            attitude: "hot-headed"
           }
           ]
-        }
+        },
 
-        }
-      ]
-   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
-        :name "Steven",
-        :age "30",
-        :attitude "confused"
+        name: "Steven",
+        age: "30",
+        attitude: "confused"
       }
-        :name "Nurse"
-        :age "30"
-        :attitude "worried"
+        name: "Nurse"
+        age: "30"
+        attitude: "worried"
       ]
    }
 }
-
-
 
 end
