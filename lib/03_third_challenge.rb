e181cfed5511b0dd3ac62d18a0958b8ebd1b4ae8
Liz Burke <liz.burke.chi@gmail.com>
2021-01-14 20:24:23 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -20,8 +20,8 @@ def third_challenge
         age: "51"
         },
       :heroine => {
-        name: "Juliet"
-        age: "15"
+        name: "Juliet",
+        age: "15",
         status: "alive"
         },
       :heroine_friends => []
